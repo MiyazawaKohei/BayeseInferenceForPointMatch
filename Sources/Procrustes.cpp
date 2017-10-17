79a0cfb9f141418079bb6c33f95cb730cdc131c9
MiyazawaKohei <miyazawa-kohei0214@g.ecc.u-tokyo.ac.jp>
2017-10-17 19:56:21 +0900

    データの読み込みを関数として分離・Eigenをレポジトリから外す.

--- Sources/Procrustes.cpp
+++ Sources/Procrustes.cpp
@@ -10,17 +10,15 @@
 #include <functional>
 using namespace std;
 
-int main(){
-	const int m=40;
-	const int n=63;
-
+vector<double*> GetDataofProtein(int m, const char* linkofData){
 	int SeqNo;
 	int Grp;
 	double x,y,z;
 	FILE *fp;
-
-	fp=fopen("../DataSet/Protein1Green","r");
-	if(fp==NULL)	return EXIT_FAILURE;
+	vector<double*> Protein;
+	//fp=fopen("../DataSet/Protein1Green","r");
+	fp=fopen(linkofData,"r");
+	if(fp==NULL)		cout<<"File is not found"<<endl;
 
 	for(int i=0; i<m; i++){
 		fscanf(fp,"%d",&SeqNo);
@@ -28,8 +26,21 @@ int main(){
 		fscanf(fp,"%lf",&x);
 		fscanf(fp,"%lf",&y);
 		fscanf(fp,"%lf",&z);
-		cout<<x<<y<<z<<endl;
+		Protein.push_back(new double [3]);
+		Protein.back()[0]=x;
+		Protein.back()[1]=y;
+		Protein.back()[2]=z;
 	}
 	fclose(fp);
+	return Protein;
+}
+
+int main(){
+	const int m=40;
+	const int n=63;
+	vector<double*> Protein1;
+	vector<double*> Protein2;
+	Protein1=GetDataofProtein( m, "../DataSet/Protein1Green");
+	Protein2=GetDataofProtein( n, "../DataSet/Protein2Green");
 	return 0;
 }
