99f0cf7081d0a43eb2eee5050844620c8b7e34d6
MiyazawaKohei <miyazawa-kohei0214@g.ecc.u-tokyo.ac.jp>
2017-10-19 20:43:09 +0900

    assignment(old)の削除,assignment2をassignmentにrename

--- Sources/Dijkstra/assignment.cpp
+++ Sources/Dijkstra/assignment.cpp
@@ -4,108 +4,103 @@
 #include <vector>
 #include "dijkstra.h"
 using namespace std;
+
 graph *graph_input_from_biparticle(int n,vector<double*> edgecost_between_biparticle)
 {
   graph *G;
-  int j;
   double x;
-  dlobj *obj;
+  //File* fin=fopen("graph.txt","r");
+  //mymalloc(G, 1);
   G = new graph[1];if ((G)==NULL) {printf("not enough memory\n"); exit(1);}
+  //fscanf(fin, "%d %d", &n, &m);
   
   G->n = 2*n+2;
   G->m = n*n+2*n;
+  dlobj *obj;
+  //mymalloc(G->E, n);
   G->E = new dlist*[G->n];if ((G->E)==NULL) {printf("not enough memory\n"); exit(1);}
-
-  for (int i=0; i<n; i++) {
-  	G->E[i] = dlist_new();
-  	G->E[n+i] = dlist_new();
-  } 
-  G->E[2*n] = dlist_new();  //start point
-  G->E[2*n+1] = dlist_new(); //goal point
+	for (int i=0; i<G->n; i++){
+		G->E[i] = dlist_new();
+	} 
   for (int i=0; i<n; i++) {
     for(int j=0; j<n; j++) {
-    	if(edgecost_between_biparticle[i][j]>=0){
-      		obj = dlobj_new(n+j+1, edgecost_between_biparticle[i][j]);
-    		dlist_append(G->E[i], obj);
-      	}else{
-      		obj = dlobj_new(i+1, edgecost_between_biparticle[i][j]);
-      		dlist_append(G->E[n+j], obj);
-      	}
+      obj = dlobj_new(n+j+1, edgecost_between_biparticle[i][j]);
+      dlist_append(G->E[i], obj);
     }
   }
   for (int i=0; i<n; i++) {
     obj = dlobj_new(i+1, 0);
     dlist_append(G->E[2*n], obj);
     obj = dlobj_new(2*n+2, 0);
-    
     dlist_append(G->E[n+i], obj);
   }
+
   return G;
 }
-
-void FlipEdge(int n, vector<double*>* edgecost_between_biparticle, int* parent){
-	int varticleNonow=2*n+2;
-	int i;
-	while(varticleNonow!=2*n+1){
-		printf("%d %d",varticleNonow,parent[varticleNonow]);
-		scanf("%d",&i);
-		if(parent[varticleNonow]!=2*n+1)return;
-		if(varticleNonow!=2*n+2 && parent[varticleNonow]!=2*n+1){
-			if(parent[varticleNonow]<varticleNonow){
-				printf("t");
-				scanf("%d",&i);
-				*edgecost_between_biparticle[parent[varticleNonow]-1][varticleNonow-n-1]*=-1;
-			}else{
-				*edgecost_between_biparticle[varticleNonow-1][parent[varticleNonow]-n-1]*=-1;
-			}
-		}
-		varticleNonow=parent[varticleNonow];
+void FlipEdges(int n, graph *G, int *parent, dlobj **fromedge){
+	int varticlenow=2*n+2;
+	dlobj *obj;
+	while(parent[varticlenow]!=-1){
+		printf("%d %d\n",varticlenow,parent[varticlenow]);
+		obj = dlobj_new(parent[varticlenow], -(fromedge[varticlenow]->w));
+    dlist_append(G->E[varticlenow-1], obj);
+		dlist_delete(fromedge[varticlenow]);
+		varticlenow=parent[varticlenow];
 	}
-	return;
 }
-int main(){
-  int n=10;
-	vector<double*> edgecost_between_biparticle;
-	graph *G;
+int main(int argc, char *argv[])
+{
+  graph *G;
   double *dist;
-	int *parent;
-  dist = new double[2*n+2+1];if ((dist)==NULL) {printf("not enough memory\n"); exit(1);}
-  parent = new int[2*n+2+1];if ((parent)==NULL) {printf("not enough memory\n"); exit(1);}
-  
+  int *parent;
+  dlobj** fromedge;
+  int i;
+  int n=4;
+	vector<double*> edgecost_between_biparticle;
 
 	for(int i=0; i<n; i++){
 		edgecost_between_biparticle.push_back(new double [n]);
 		for(int j=0; j<n; j++){
 			//edgecost_between_biparticle.back()[j]=(i==j?0:1);
-			edgecost_between_biparticle[i][j]=(i==j?0.1:1);
+			edgecost_between_biparticle[i][j]=(i+j)%3;//(i==j?i+1:100);
 		}
 	}
 
-	while(1){
-		G=graph_input_from_biparticle(n,edgecost_between_biparticle);
-		Dijkstra(G, 2*n+1, dist, parent);
-		if(dist[2*n+1]>10000) break;
-		for (int i=1; i<=G->n; i++) {
-	    printf("p(%d)=%d dist=%lf\n", i, parent[i], dist[i]);
-	 	 }
-
-		int varticleNonow=2*n+2;
-		printf("%d",n);
-		while(varticleNonow!=2*n+1){
-			if(varticleNonow!=2*n+2 && parent[varticleNonow]!=2*n+1){
-				if(parent[varticleNonow]<varticleNonow){
-					edgecost_between_biparticle[parent[varticleNonow]-1][varticleNonow-n-1]*=-1;
-				}else{
-					edgecost_between_biparticle[varticleNonow-1][parent[varticleNonow]-n-1]*=-1;
-				}
-			}
-			varticleNonow=parent[varticleNonow];
-		}
-		graph_free(G);
-		//G=graph_input_from_biparticle(n,edgecost_between_biparticle);
-	}
-
+  G = graph_input_from_biparticle( n,edgecost_between_biparticle);
+  dist = new double[G->n+1];if ((dist)==NULL) {printf("not enough memory\n"); exit(1);}
+  parent = new int[G->n+1];if ((parent)==NULL) {printf("not enough memory\n"); exit(1);}
+  
+  while(1){ 
+  	for(int i=0; i<2*n+2; i++){
+	  	dlobj* obj=G->E[i]->nil->next;
+	  	while(obj!=G->E[i]->nil){
+	  		printf("%d %d\n",i,obj->v);
+	  		obj=obj->next;
+	  	}
+  	}
+  	fromedge = new dlobj*[G->n+1];if ((parent)==NULL) {printf("not enough memory\n"); exit(1);}
+	  Dijkstra(G, 2*n+1, dist, parent,fromedge);
+	  if(dist[2*n+2]>10000) break;
+	  for (i=1; i<=G->n; i++) {
+	    printf("p(%d)=%d dist=%lf,", i, parent[i], dist[i]);
+	    printf("p(%d)=%lf\n", i, (fromedge[i]!=NULL?fromedge[i]->w:-1));
+	  }
+	  FlipEdges(n,G,parent,fromedge);
+	  delete [] fromedge;
+  }
+  for(int i=0; i<2*n+2; i++){
+  	dlobj* obj=G->E[i]->nil->next;
+  	while(obj!=G->E[i]->nil){
+  		printf("%d %d\n",i,obj->v);
+  		obj=obj->next;
+  	}
+  }
+  for(int i=n; i<2*n; i++){
+  	printf("%d %d\n",i+1-n,G->E[i]->nil->next->v);
+  }
   delete [] parent;
   delete [] dist;
+  graph_free(G);
   return 0;
 }
+
